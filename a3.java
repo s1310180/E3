cc4cd6561e9284b696c9d5d7b05771438c765f30
s1310180 <s1310180@u-aizu.ac.jp>
2024-07-04 16:50:36 +0900

    User name added

--- a3.java
+++ a3.java
@@ -1,7 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class a3{
     public static void main(String[] args){
+        System.out.println("Who are you?");
+        Scanner s = new Scanner(System.in);
+        System.out.print(">");
+        String name = s.nextLine();
+        System.out.println("Hello, "+name+"!");
     System.out.println("Tossing a coin...");
 
     Random rand = new Random();
