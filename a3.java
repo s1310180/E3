24782d72aaf86aa7ee0a5ff5ae6ffc4c9be5af20
s1310180 <s1310180@u-aizu.ac.jp>
2024-07-04 16:46:20 +0900

    HoT: initial revision

---
+++ a3.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+class a3{
+    public static void main(String[] args){
+    System.out.println("Tossing a coin...");
+
+    Random rand = new Random();
+    String[] coins = {"Heads", "Tails"};
+
+    int p=0;
+    int q=0;
+
+    for(int i=1;i<=3;i++){
+        String coin = coins[rand.nextInt(2)];
+        System.out.println("Round "+i+": "+coin);
+        if(coin=="Heads"){
+            p++;
+        }else{
+            q++;
+        }
+    }
+
+    System.out.print("Heads: "+p);
+    System.out.print(", ");
+    System.out.println("Tails: "+q);
+
+    }
+}
